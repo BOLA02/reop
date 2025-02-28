5249b1b1722a62e3ac061eabca5fd5c1fab7c008
iambola <kamilamoho2020@gmail.com>
2025-02-28 18:11:59 +0100

    dsajfva

---
+++ js/cript.js
@@ -0,0 +1,17 @@
+"use strict"
+// document.addEventListener("DOMContentLoaded", function () {
+//     const options = {
+//         root: null,
+//         threshold: 0.2,
+//     };
+//     const observer = new IntersectionObserver((entries) => {
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 entry.target.classList.add("visible");
+//             }
+//         });
+//     }, options);
+//     document.querySelectorAll(".column").forEach(col => {
+//         observer.observe(col);
+//     });
+// });
